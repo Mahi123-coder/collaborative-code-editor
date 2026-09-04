bde6b5190f0b1df964eb7db9d4e04262f929ba46
Agrani <agraniabha2@gmail.com>
2026-09-04 20:46:27 +0530

    or

--- server/server.js
+++ server/server.js
@@ -264,11 +264,11 @@ mongoose
   .then(() => {
     console.log("MongoDB connected successfully");
 
-    server.listen(5000, () => {
-      console.log(
-        "Server running on http://localhost:5000"
-      );
-    });
+    const PORT = process.env.PORT || 5000;
+
+server.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running on port ${PORT}`);
+});
   })
   .catch((error) => {
     console.error(
